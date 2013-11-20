418897ca94be045d58de35cf0914e07aadb6389b
daniel gutson <daniel.gutson@tallertechnologies.com>
2013-11-20 16:39:38 -0300

    GPL stuff and headings and text files.

--- test.cpp
+++ test.cpp
@@ -1,6 +1,29 @@
-/*
-    Copyright (C) Daniel Gutson 2013
-*/
+/**
+ * \file  test.cpp
+ * \brief Basic tests for demonstration.
+ *
+ * Statements-templates: Extending expression templates to statements and flow control
+ * <http://statement-templates.googlecode.com/>
+ * Copyright (C) 2013 Daniel Gutson, Taller Technologies
+ *
+ * This file is part of the Statement-templates project.
+ *
+ * Author:         Daniel Gutson
+ *
+ * Statement-tamplets is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Statement-tamplets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Statement-tamplets.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include "basicStatements.h"
 #include "parallelFor.h"
