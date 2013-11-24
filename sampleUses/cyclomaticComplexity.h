93f8632e231ca80ceed5f9da1d756ff8a8a7565e
Hugo Arregui <hugo.arregui@gmail.com>
2013-11-23 22:02:15 -0300

    contributor, gpl header, makefile target for generating tgz

--- sampleUses/cyclomaticComplexity.h
+++ sampleUses/cyclomaticComplexity.h
@@ -1,9 +1,37 @@
+/**
+ * \file  cyclomaticComplexity.h
+ * \brief Cyclomatic Complexity sample.
+ *
+ * Statements-templates: Extending expression templates to statements and flow control
+ * <http://statement-templates.googlecode.com/>
+ * Copyright (C) 2013 Daniel Gutson, Taller Technologies
+ *
+ * This file is part of the Statement-templates project.
+ *
+ * Author:        Hugo Arregui 
+ *
+ * Statement-tamplets is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Statement-tamplets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Statement-tamplets.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 template <int a, int b>
 struct Max
 {
     static const int value = a > b ? a : b;
 };
 
+// Based on: http://users.csc.calpoly.edu/~jdalbey/206/Lectures/BasisPathTutorial/index.html
 template <class T>
 struct CyclomaticComplexity
 {
