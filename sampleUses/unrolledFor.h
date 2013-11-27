773843c386ab59697d048d7df4860ed8a1721e27
Hugo Arregui <hugo.arregui@gmail.com>
2013-11-27 20:14:10 -0300

    update contributors

--- sampleUses/unrolledFor.h
+++ sampleUses/unrolledFor.h
@@ -8,7 +8,7 @@
  *
  * This file is part of the Statement-templates project.
  *
- * Author:         Daniel Gutson
+ * Author:         Daniel Gutson, Hugo Arregui
  *
  * Statement-tamplets is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
